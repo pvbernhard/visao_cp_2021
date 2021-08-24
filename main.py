48e460dcbe35e49c864026b675fd90ee17bb3314
Pedro V. Bernhard <pvbernhard@gmail.com>
2021-08-24 03:35:47 -0300

    init streamlit

---
+++ main.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import my_functions as myf
+
+import numpy as np
+from sklearn import preprocessing
+
+
+DATASET_FOLDER = 'Dataset'
+
+feature_extractors = ['GLCM', 'ORB', 'Gabor']
+classificators = ['XGBoost', 'Bagging']
+
+FEATURE_EXTRACTOR = feature_extractors[0]
+CLASSIFICATOR = classificators[0]
+
+SIZE = 256
+
+# GLCM
+
+DISTANCES = [1, 3, 5]
+ANGLES = [0, np.pi/4, np.pi/2] # [0, np.pi/4] # np.pi/2, 3*np.pi/4, np.pi
+
+# ORB
+
+N_KEYPOINTS = 10
+
+# Gabor
+
+KSIZE = 20  #Use size that makes sense to the image and fetaure size. Large may not be good. 
+#On the synthetic image it is clear how ksize affects imgae (try 5 and 50)
+SIGMA = 20 #Large sigma on small features will fully miss the features. 
+THETA = 1*np.pi/4  #1/4 shows horizontal 3/4 shows other horizontal. Try other contributions
+LAMBDA = 1*np.pi/4  #1/4 works best for angled. 
+GAMMA = 0.4  #Value of 1 defines spherical. Calue close to 0 has high aspect ratio
+#Value of 1, spherical may not be ideal as it picks up features from other regions.
+PHI = 0  #Phase offset. I leave it to 0.
+REDUCE_DIM = 8
+
+# Cross-Validation
+
+N_SPLITS = 5
+
+# PSO
+
+OPTIONS = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+N_PARTICLES = 15
+DIMENSIONS = 5
+X_MAX = 1 * np.ones(DIMENSIONS)
+X_MAX[1:] *= 50
+X_MIN = 0.01 * np.ones(DIMENSIONS)
+BOUNDS = (X_MIN, X_MAX)
+ITERS = 5
+
+
+st.set_page_config(
+  page_title='Trabalho de Visão CP',
+  page_icon='💻',
+  layout='wide',
+  initial_sidebar_state='expanded'
+)
+
+with st.sidebar.form(key='form'):
+  st.markdown('# Opções')
+  FEATURE_EXTRACTOR = st.selectbox('Escolha o extrator de características', feature_extractors)
+  CLASSIFICATOR = st.selectbox('Escolha o classificador', classificators)
+  SIZE = st.number_input('Tamanho das imagens', 128, 460, 256)
+
+  st.markdown('## Parâmetros')
+  if FEATURE_EXTRACTOR == 'GLCM':
+    DISTANCES = st.multiselect('GLCM: Distâncias', [1, 3, 5])
+    ANGLES = st.multiselect('GLCM: Ângulos', [0, np.pi/4, np.pi/2])
+  elif FEATURE_EXTRACTOR == 'ORB':
+    N_KEYPOINTS = st.slider('ORB: Número de keypoints', 1, 100, 10)
+  elif FEATURE_EXTRACTOR == 'Gabor':
+    KSIZE = st.slider('ORB: Ksize', 1, 100, 20)
+    SIGMA = st.slider('ORB: Sigma', 1, 100, 20)
+    THETA = st.slider('ORB: Theta', 0.01, 2*np.pi, 1*np.pi/4)
+    LAMBDA = st.slider('ORB: Lambda', 0.01, 2*np.pi, 1*np.pi/4)
+    GAMMA = st.slider('ORB: Gamma', 0.0, 1.0, 0.4, 0.05)
+    PHI = st.slider('ORB: Phi', 0.0, 1.0, 0.0, 0.05)
+    REDUCE_DIM = st.number_input('ORB: Reduzir dimensões', 1, 32, 8)
+
+  st.markdown('## PSO')
+  N_PARTICLES = st.slider('Número de partículas', 1, 100, 15)
+  ITERS = st.slider('Iterações', 1, 100, 5)
+
+submit_button = st.sidebar.form_submit_button(label='Iniciar')
+
+if submit_button:
+  bar_dataset = st.progress(0)
+
+  X, y_strings = myf.load_dataset(DATASET_FOLDER, progress_bar=bar_dataset)
+
+  le = preprocessing.LabelEncoder()
+  le.fit(y_strings)
+  y = le.transform(y_strings)
+
+  st.markdown('Exemplos:')
+  for i in range(3):
+    n = random.randint(0, X.shape[0]-1)
+    img = X_base[n]
+    st.image(img)
+
+  bar_features = st.progress(0)
+
+  X_features = myf.feature_extractor(FEATURE_EXTRACTOR, X, progress_bar=bar_features)
+
+  X_for_training = np.array(X_features)
+
+  myf.main_fit(X_for_training, y)
---
+++ my_functions.py
@@ -0,0 +1,307 @@
+
+def pre_process(img, size):
+  import cv2
+  img = cv2.resize(img, (size, size))
+  img = cv2.GaussianBlur(img, (5, 5), 0)
+  return img
+
+
+def feature_extractor_GLCM(dataset, progress_bar=None):
+  import streamlit as st
+  import pandas as pd
+  from skimage.feature import greycomatrix, greycoprops
+
+  st.markdown(f'Extraindo características...')
+  feat_dataset = pd.DataFrame()
+  
+  n = 0
+  for image_n in range(dataset.shape[0]):
+    if progress_bar:
+      progress_bar.progress((n / dataset.shape[0])*100)
+    
+    df = pd.DataFrame()
+    img = dataset[image_n, :, :]
+
+    distances = DISTANCES
+    angles = ANGLES
+
+    i = 0
+    for distance in distances:
+      for angle in angles:
+        GLCM = greycomatrix(img, [distance], [angle])       
+        GLCM_Energy = greycoprops(GLCM, 'energy')[0]
+        df['Energy' + str(i)] = GLCM_Energy
+        GLCM_corr = greycoprops(GLCM, 'correlation')[0]
+        df['Corr' + str(i)] = GLCM_corr       
+        GLCM_diss = greycoprops(GLCM, 'dissimilarity')[0]
+        df['Diss_sim' + str(i)] = GLCM_diss       
+        GLCM_hom = greycoprops(GLCM, 'homogeneity')[0]
+        df['Homogen' + str(i)] = GLCM_hom       
+        GLCM_contr = greycoprops(GLCM, 'contrast')[0]
+        df['Contrast' + str(i)] = GLCM_contr
+        i += 1
+    
+    feat_dataset = feat_dataset.append(df)
+    n += 1
+  st.markdown(f'Shape: {feat_dataset.shape}')
+  return feat_dataset
+
+
+def feature_extractor_ORB(dataset, progress_bar=None):
+  import streamlit as st
+  import pandas as pd
+  import cv2
+
+  st.markdown('Extraindo características...')
+  feat_dataset = pd.DataFrame()
+  
+  n = 0
+  for image_n in range(dataset.shape[0]):
+    if progress_bar:
+      progress_bar.progress((n / dataset.shape[0])*100)
+    
+    img = dataset[image_n, :, :]
+
+    orb = cv2.ORB_create(N_KEYPOINTS)
+    kp, des = orb.detectAndCompute(img, None)
+    des = des.reshape(-1)
+    df = pd.DataFrame(des)
+    df = df.transpose()
+
+    df /= 255.
+
+    feat_dataset = feat_dataset.append(df)
+    n += 1
+  st.markdown(f'Shape: {feat_dataset.shape}')
+  return feat_dataset
+
+
+def feature_extractor_GABOR(dataset, progress_bar=None):
+  import streamlit as st
+  import pandas as pd
+  import cv2
+
+  st.markdown('Extraindo características...')
+  feat_dataset = pd.DataFrame()
+
+  ksize = KSIZE
+  sigma = SIGMA
+  theta = THETA
+  lamda = LAMBDA
+  gamma = GAMMA
+  phi = PHI
+
+  kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, phi, ktype=cv2.CV_32F)
+  
+  n = 0
+  for image_n in range(dataset.shape[0]):
+    if progress_bar:
+      progress_bar.progress((n / dataset.shape[0])*100)
+    
+    img = dataset[image_n, :, :]
+    
+    fimg = cv2.filter2D(img, cv2.CV_8UC3, kernel)
+
+    # reduzindo tamanho pra ter menos caracteristicas
+    # pra reduzir o tempo
+    fimg = cv2.resize(fimg, (SIZE // REDUCE_DIM, SIZE // REDUCE_DIM))
+
+    fimg = fimg.reshape(-1)
+    df = pd.DataFrame(fimg)
+    df = df.transpose()
+
+    df /= 255.
+
+    feat_dataset = feat_dataset.append(df)
+    n += 1
+  st.markdown(f'Shape: {feat_dataset.shape}')
+  return feat_dataset
+
+
+def feature_extractor(feature_extractor, dataset, progress_bar=None):
+  if feature_extractor == 'GLCM':
+    return feature_extractor_GLCM(dataset, progress_bar)
+  if feature_extractor == 'ORB':
+    return  feature_extractor_ORB(dataset, progress_bar)
+  if feature_extractor == 'Gabor':
+    return  feature_extractor_GABOR(dataset, progress_bar)
+  return None
+
+
+def model_prediction(params, X_train, y_train, X_test):
+  import xgboost as xgb
+
+  model = xgb.XGBClassifier(
+      use_label_encoder=False,
+      eval_metric='logloss',
+      eta=params[0], # range: [0,1]
+      gamma=params[1], # range: [0,inf]
+      max_depth=int(params[2]), # range:[+0,inf]
+      min_child_weight=params[3], # range: [0,inf]
+      max_delta_step=params[4], # range: [0,inf]
+      )
+  model.fit(X_train, y_train)
+  prediction = model.predict(X_test)
+  return prediction
+
+
+def model_prediction_bagging(params, X_train, y_train, X_test):
+  from sklearn.tree import DecisionTreeClassifier
+  from sklearn.ensemble import BaggingClassifier
+
+  base_cls = DecisionTreeClassifier(max_depth=int(params[1]),
+                                    min_samples_split=params[2],
+                                    min_samples_leaf=params[3]
+                                    )
+  num_trees = int(params[4])
+  model = BaggingClassifier(base_estimator = base_cls,
+                            n_estimators = num_trees,
+                            random_state = 42)
+  model.fit(X_train, y_train)
+  prediction = model.predict(X_test)
+  return prediction
+
+
+def xgboost_cost(params, X_train, y_train, X_test, y_test):
+  import numpy as np
+  from sklearn import metrics
+
+  results = []
+  num_rows = np.shape(params)[0]
+
+  params_to_use = params.copy()
+
+  for i in range(num_rows):
+    prediction = model_prediction(params_to_use[i], X_train, y_train, X_test)
+    mean_error = metrics.mean_absolute_error(y_test, prediction)
+    
+    results.append(mean_error)
+
+  return np.transpose(results)
+
+
+def bagging_cost(params, X_train, y_train, X_test, y_test):
+  import numpy as np
+  from sklearn import metrics
+
+  results = []
+  num_rows = np.shape(params)[0]
+
+  params_to_use = params.copy()
+
+  for i in range(num_rows):
+    prediction = model_prediction_bagging(params_to_use[i], X_train, y_train, X_test)
+    mean_error = metrics.mean_absolute_error(y_test, prediction)
+    
+    results.append(mean_error)
+
+  return np.transpose(results)
+
+
+def load_dataset(dataset_folder, progress_bar=None):
+  import os, glob, cv2
+
+  X = []
+  y = []
+
+  total = 0
+  for directory_path in glob.glob(os.path.join(dataset_folder, '*')):
+    total += len(glob.glob(os.path.join(directory_path, "*.png")))
+
+  counter = 0
+  for directory_path in glob.glob(os.path.join(dataset_folder, '*')):
+      label = directory_path.split(os.path.sep)[-1]
+      for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+          img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+          img = pre_process(img, SIZE)
+          X.append(img)
+          y.append(label)
+          counter += 1
+          if progress_bar:
+            progress_bar.progress((counter / total)*100)
+
+  X = np.array(X)
+  y = np.array(y)
+  return X, y
+
+
+def main_fit(X, y):
+  import streamlit as st
+  from sklearn.model_selection import StratifiedKFold
+  from pyswarms.single.global_best import GlobalBestPSO
+  from sklearn import metrics
+
+  skf = StratifiedKFold(n_splits=N_SPLITS, random_state=42, shuffle=True)
+  st.markdown(f'{skf}')
+
+  accuracies = []
+  mean_errors = []
+
+  fold = 1
+  for train_index, test_index in skf.split(X, y):
+
+    st.markdown(f'Fold {fold}')
+
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    X_train = X_train.reshape(X_train.shape[0], -1)
+
+    optimizer = GlobalBestPSO(n_particles=N_PARTICLES,
+                              dimensions=DIMENSIONS,
+                              options=OPTIONS,
+                              bounds=BOUNDS)
+    
+    if CLASSIFICATOR == 'XGBoost':
+      cost, pos = optimizer.optimize(xgboost_cost,
+                                    iters=ITERS,
+                                    X_train=X_train,
+                                    y_train=y_train,
+                                    X_test=X_test,
+                                    y_test=y_test
+                                    )
+      
+      st.markdown(f'Parâmetros usados:')
+      st.markdown(f'- max_depth={pos[1]}')
+      st.markdown(f'- min_samples_split={pos[2]}')
+      st.markdown(f'- min_samples_leaf={pos[3]}')
+      st.markdown(f'- max_delta_step={pos[4]}')
+
+      prediction = model_prediction(pos, X_train, y_train, X_test)
+
+    elif CLASSIFICATOR == 'Bagging':
+      cost, pos = optimizer.optimize(bagging_cost,
+                                    iters=ITERS,
+                                    X_train=X_train,
+                                    y_train=y_train,
+                                    X_test=X_test,
+                                    y_test=y_test
+                                    )
+      
+      st.markdown(f'Parâmetros usados:')
+      st.markdown(f'- eta={pos[0]}')
+      st.markdown(f'- gamma={pos[1]}')
+      st.markdown(f'- max_depth={pos[2]}')
+      st.markdown(f'- min_child_weight={pos[3]}')
+      st.markdown(f'- num_trees={pos[4]}')
+
+      prediction = model_prediction_bagging(pos, X_train, y_train, X_test)
+    else:
+      raise Exception('Erro: classificador não encontrado.')
+
+    acc = metrics.accuracy_score(y_test, prediction)
+    log_loss = metrics.log_loss(y_test, prediction)
+    mean_error = metrics.mean_absolute_error(y_test, prediction)
+    st.markdown(f'Accuracy: {acc}')
+
+    accuracies.append(acc)
+    mean_errors.append(mean_error)
+
+    st.markdown(metrics.classification_report(y_test, prediction, target_names=['benign', 'malignant']))
+
+    st.markdown('---')
+
+    fold += 1
+
+  st.markdown(f'Média acc: {statistics.mean(accuracies)}')
+  st.markdown(f'Média mean_error: {statistics.mean(mean_errors)}')
